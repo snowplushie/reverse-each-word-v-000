175f6269ced479fbe42f4f59fe6a1239468f2422
Paige Wiener <snowplushie@yahoo.com>
2019-03-02 05:29:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 sentence1=["Hello", "there,", "and", "how", "are", "you?"]
 
 def reverse_each_word(sentence1)
-  reverse_each_word.each do |sentence1|
+  reverse_each_word.reverse_each 
   end
 end
