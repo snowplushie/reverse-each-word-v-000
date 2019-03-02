d97ec11fca8f285af4a88e8318e39ad482ffe610
Paige Wiener <snowplushie@yahoo.com>
2019-03-02 05:34:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 sentence1=["Hello", "there,", "and", "how", "are", "you?"]
 
-def reverse_each_word(sentence1)
+def reverse_each_word(sentence)
   sentence.split.each {|word| word.reverse}.join(" ")
 end
