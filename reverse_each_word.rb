dd45773eae6592cd5ad73d19aa6c22c1388808d6
Paige Wiener <snowplushie@yahoo.com>
2019-03-02 05:37:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
-sentence1=["Hello" "there,", "and", "how", "are", "you?"]
-sentence2=["Hi", "again,", "just", "making", "sure", "it's", "reversed!"]
-
 def reverse_each_word(sentence)
-  sentence.split.each {|word| word.reverse}.join(" ")
+ array=sentence.split
+ 
 end
